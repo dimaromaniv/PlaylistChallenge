17e1a8ad1d14b14a3737f20332184566e921f5a7
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-17 20:06:11 -0400

    task done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,15 +1,36 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+
+    public Integer selection(Integer startIndex, String selection) {
+
+        ArrayList<String> listPlayList = new ArrayList(Arrays.asList(playList));
+        int counterForvard = 0;
+        int counterRevers  = 0;
+        int currentIndex = startIndex;
+
+       while (!playList[currentIndex].contains(selection)) {
+           counterForvard++;
+           currentIndex = (currentIndex + 1 ) % playList.length;
+        }
+       currentIndex = startIndex;
+        while (!playList[currentIndex].contains(selection)) {
+           counterRevers++;
+           currentIndex = (currentIndex - 1 + playList.length ) % playList.length;
+       }
+
+            return Math.min(counterForvard,counterRevers);
     }
-    //jgkgljkhkjb
 }
